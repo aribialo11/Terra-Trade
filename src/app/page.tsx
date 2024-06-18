941c75010ef71946a349c15e12f165c21855685a
julicarabaj <127246404+julicarabaj@users.noreply.github.com>
2024-06-18 11:56:43 -0300

    page tsx bien

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,46 @@
+"use client";
+
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
+const SUPABASE_URL = 'https://ncnjbkwnzmbroxedwour.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jbmpia3duem1icm94ZWR3b3VyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MDU0NTUsImV4cCI6MjAzMjQ4MTQ1NX0.jGuUnZmvbpltBbLfgQzRLFK216C4ff_37aGDJ3V4bvQ';
+
+type Agencia = {
+  id_agencias: Number;
+  nombre: String;
+  direccion: String;
+}
 
 const Page = () => {
-  return (
-    <main>
-      <header>
-        <div className="logo">
-            <h1 id="volverpi" className="terratrade">TerraTrade</h1>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Servicios</a></li>
-                <li><a href="#">Acerca de nosotros</a></li>
-                <li><a href="#">Perfil</a></li>
-                <li><a href="#">Preguntas frecuentes</a></li>
-            </ul>
-        </nav>
-        <div className="login-btn">
-            <button>Login</button>
-        </div>
-      </header>
-      <div className="imagen-y-frase">
-        <div className="imagen">
-            <img src="../imagenes/logo1.png" alt="Logo" />
-        </div>
-        <div className="frase">
-            <h1>Empieza a invertir <br /> en terrenos <br /> desde la comodidad <br /> de tu casa</h1>
-            <div className="comenzar-btn">
-                <button id="volverpp" className="volver">Comenzar</button>
-            </div>
-        </div>
-      </div>
+  const [agencias, setAgencias] = useState<Agencia[]>([]);
 
-      {/* Modal de inicio de sesión */}
-      <div id="loginModal" className="modal">
-        <div className="modal-content">
-          <span className="close">&times;</span>
-          <div className="formulario">
-            <h1>Inicio de sesión</h1>
-            <form action="#" method="GET">
-              <div className="username">
-                <input type="text" name="DNI" id="DNI" placeholder="Ingrese su nombre de usuario" required />
-                <label>Usuario</label>
-              </div>
-              <div className="contrasena">
-                <input type="password" name="Contraseña" id="Contrasenia" placeholder="Ingrese su contraseña" required />
-                <label>Contraseña</label>
-              </div>
-              <input type="button" value="Iniciar" id="volverpp" className="boton" />
-              <div className="registro">
-                <p>¿No tenés cuenta? <a id="registrate" className="registrate">Registrate acá</a></p>
-              </div>
-            </form>
-          </div>
+  useEffect(() => {
+    fetch(`${SUPABASE_URL}/rest/v1/agencias`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'apikey': SUPABASE_KEY,
+        'Authorization': `Bearer ${SUPABASE_KEY}`
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAgencias(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {agencias.map((agencia) => (
+        <div key={agencia.id_agencias.toString()}>
+          <h2>{agencia.nombre}</h2>
+          <p>{agencia.direccion}</p>
         </div>
-      </div>
-    </main>
-  );
-};
+      ))}
+    </div>
+  );};
 
 export default Page;
