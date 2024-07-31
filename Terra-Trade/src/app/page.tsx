dc277627a49938916850134f2c5b28f685a83d7f
aribialo11 <127206610+aribialo11@users.noreply.github.com>
2024-07-31 14:30:23 -0300

    cambie dos boludeces de la PP

--- Terra-Trade/src/app/page.tsx
+++ Terra-Trade/src/app/page.tsx
@@ -23,16 +23,16 @@ const Home = () => {
         <title>Home</title>
       </Head>
       <div className="navbar" style={{ position: 'absolute', top: '0', width: '100%', display: 'flex', justifyContent: 'space-between', padding: '20px', backgroundColor: '#20493C' }}>
-        <div style={{ color: 'white', fontSize: '24px', fontWeight: 'bold' }}>
+        <div style={{ color: 'white', fontSize: '24px', fontWeight: 'bold' , cursor:'pointer' }}>
           TerraTrade
         </div>
-        <div style={{ display: 'flex', gap: '20px', color: 'white' }}>
+        <div style={{ display: 'flex', gap: '20px', color: 'white' , cursor:'pointer' }}>
           <span>Home</span>
           <span>Servicios</span>
           <span>Acerca de nosotros</span>
           <span>Perfil</span>
           <span>Preguntas frecuentes</span>
-          <button style={{ backgroundColor: '#9DBFA4', border: 'none', padding: '10px 20px', borderRadius: '5px', color: 'white' }}>Comenzar</button>
+          <button style={{ backgroundColor: '#9DBFA4', border: 'none', padding: '10px 20px', borderRadius: '5px', color: 'white' }}>Log-in</button>
         </div>
       </div>
       <div className="imagen-y-frase" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '80%' }}>
@@ -40,14 +40,14 @@ const Home = () => {
           <Image
             src="/imagenes/logo.png"
             alt="Logo"
-            width={500}
-            height={500}
+            width={1000}
+            height={1000}
           />
         </div>
         <div className="frase" style={{ flex: 1, color: 'white', textAlign: 'right' }}>
           <h1 style={{ fontSize: '3rem' }}>Empieza a invertir en <br /> terrenos desde la <br /> comodidad de tu casa</h1>
           <div className="comenzar-btn" style={{ marginTop: '20px' }}>
-          <button onClick={handleOpenModal}>Comenzar</button>
+          <button style={{ backgroundColor: '#9DBFA4', border: 'none', padding: '10px 20px', borderRadius: '5px', color: 'white' }} onClick={handleOpenModal}>Comenzar</button>
           </div>
         </div>
       </div>
