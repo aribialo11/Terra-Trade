235e6024c21e9f4cba086b84feab8eeaa54d958c
aribialo11 <127206610+aribialo11@users.noreply.github.com>
2024-08-07 14:50:51 -0300

    inicio de sesion

--- Terra-Trade/src/app/page.tsx
+++ Terra-Trade/src/app/page.tsx
@@ -3,13 +3,11 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import RootLayout from './layout';
-import Modal from '../../componentes/modal'; 
 
 const Home = () => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [isLoginModalOpen, setLoginModalOpen] = useState(false);
-  const [account, setAccount] = useState('');
+
   const handleOpenModal = () => {
     setModalOpen(true);
   };
@@ -17,22 +15,31 @@ const Home = () => {
   const handleCloseModal = () => {
     setModalOpen(false);
   };
+
+  const handleOpenLoginModal = () => {
+    setModalOpen(false);
+    setLoginModalOpen(true);
+  };
+
+  const handleCloseLoginModal = () => {
+    setLoginModalOpen(false);
+  };
+
   return (
     <div style={{ backgroundColor: '#20493C', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
       <Head>
         <title>Home</title>
       </Head>
       <div className="navbar" style={{ position: 'absolute', top: '0', width: '100%', display: 'flex', justifyContent: 'space-between', padding: '20px', backgroundColor: '#20493C' }}>
-        <div style={{ color: 'white', fontSize: '24px', fontWeight: 'bold' }}>
+        <div style={{ color: 'white', fontSize: '24px', fontWeight: 'bold', cursor: 'pointer' }}>
           TerraTrade
         </div>
-        <div style={{ display: 'flex', gap: '50px', color: 'white' }}>
+        <div style={{ display: 'flex', gap: '50px', color: 'white', cursor: 'pointer', justifyContent: 'center', flex: 1 }}>
           <span>Home</span>
           <span>Servicios</span>
           <span>Acerca de nosotros</span>
           <span>Perfil</span>
           <span>Preguntas frecuentes</span>
-          
         </div>
       </div>
       <div className="imagen-y-frase" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '80%' }}>
@@ -47,11 +54,10 @@ const Home = () => {
         <div className="frase" style={{ flex: 1, color: 'white', textAlign: 'right' }}>
           <h1 style={{ fontSize: '3rem' }}>Empieza a invertir en <br /> terrenos desde la <br /> comodidad de tu casa</h1>
           <div className="comenzar-btn" style={{ marginTop: '20px' }}>
-          <button onClick={handleOpenModal}>Comenzar</button>
+            <button style={{ backgroundColor: '#9DBFA4', border: 'none', padding: '10px 20px', borderRadius: '5px', color: 'white' }} onClick={handleOpenModal}>Comenzar</button>
           </div>
         </div>
       </div>
-      {isModalOpen && <Modal onClose={handleCloseModal} />}
     </div>
   );
 };
