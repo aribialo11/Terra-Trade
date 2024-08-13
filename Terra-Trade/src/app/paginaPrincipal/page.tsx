845acd322a40e413b6f25b9ab3e00d31220bc013
julicarabaj <127246404+julicarabaj@users.noreply.github.com>
2024-08-13 12:01:27 -0300

    funciona la coneccion???

--- Terra-Trade/src/app/paginaPrincipal/page.tsx
+++ Terra-Trade/src/app/paginaPrincipal/page.tsx
@@ -1,29 +1,15 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import Web3 from 'web3';
-import styles from '../styles/PaginaPrincipalStyles'; // Suponiendo que los estilos estén en un archivo separado
-
-// Configurar la conexión a Infura utilizando Web3.js
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`));
-
-// Obtener el balance de una dirección
-const getBalance = async (address: string) => { // Especificar el tipo string
-  try {
-    const balance = await web3.eth.getBalance(address);
-    console.log(web3.utils.fromWei(balance, 'ether'));
-  } catch (error) {
-    console.error('Error al obtener el balance:', error);
-  }
-};
+import Web3 from '../utils/web3';
 
 const PaginaPrincipal = () => {
-  const [account, setAccount] = useState<string>(''); // Especificar el tipo de estado
+  const [account, setAccount] = useState('');
 
   useEffect(() => {
     const loadAccount = async () => {
       try {
-        const accounts = await web3.eth.getAccounts();
+        const accounts = await Web3.eth.getAccounts();
         if (accounts.length > 0) {
           setAccount(accounts[0]);
         } else {
@@ -38,8 +24,9 @@ const PaginaPrincipal = () => {
   }, []);
 
   return (
-    <div className='conteiner'>
-     <p style={styles.accountText}>Cuenta conectada: {account || 'No hay cuenta conectada'}</p>
+    <div>
+      <h1>Bienvenido a TerraTrade</h1>
+      <p>Cuenta conectada: {account || 'No hay cuenta conectada'}</p>
     </div>
   );
 };
