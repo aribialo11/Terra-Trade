ca39c67e6db5bef3cdee19ad68616a147f727253
MatiasDizenhaus <127206234+MatiasDizenhaus@users.noreply.github.com>
2024-08-29 14:58:56 -0300

    presentacion

--- Terra-Trade/src/app/layout.tsx
+++ Terra-Trade/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/Navbar'
-import { Providers } from './Providers'
 
 
 
@@ -23,10 +21,7 @@ export default function RootLayout({
         <meta name="description" content={String(metadata.description ?? 'Default description')} />
       </head>
       <body> 
-        <Providers>
-        <Navbar />
         {children}
-        </Providers>
       <div className="navbar" style={{ position: 'absolute', top: '0', width: '100%', display: 'flex', justifyContent: 'space-between', padding: '20px', backgroundColor: '#20493C' }}>
         <div style={{ color: 'white', fontSize: '24px', fontWeight: 'bold', cursor: 'pointer' }}>
           TerraTrade
