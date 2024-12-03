838653467ae5f0189152b4b5c22179939694b61a
julicarabaj <127246404+julicarabaj@users.noreply.github.com>
2024-12-03 15:11:05 -0300

    vercelllll?

--- Terra-Trade/next.config.mjs
+++ Terra-Trade/next.config.mjs
@@ -1,6 +1,17 @@
-module.exports = {
-    experimental: {
-      appDir: true,
-    },
-  };
-  
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+const withPWA = require("next-pwa");
+
+module.exports = withPlugins(
+    [optimizedImages],
+    [
+      withPWA({
+        pwa: {
+          dest: "public",
+        },
+      }),
+    ],
+    {
+      distDir: '../../dist/client',
+    }
+);
