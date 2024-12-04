c168015898c2913b1295d200d3bfa747a51ff0b9
julicarabaj <127246404+julicarabaj@users.noreply.github.com>
2024-12-04 08:36:58 -0300

    actualizacionvercel

--- Terra-Trade/next.config.mjs
+++ Terra-Trade/next.config.mjs
@@ -1,17 +1,25 @@
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
-const withPWA = require("dotenv");
+import withPlugins from "next-compose-plugins";
+import nextPWA from "next-pwa";
+import path from "path";
 
-module.exports = withPlugins(
-    [optimizedImages],
-    [
-      withPWA({
-        pwa: {
-          dest: "public",
-        },
-      }),
-    ],
-    {
-      distDir: '../../dist/client',
-    }
+// Carga las variables de entorno
+import dotenv from "dotenv";
+dotenv.config({ path: path.resolve(process.cwd(), ".env.production") });
+
+const withPWA = nextPWA({
+  dest: "public",
+});
+
+// Configuración de Next.js
+const nextConfig = {
+  distDir: "../../dist/client",
+};
+
+// Exporta la configuración usando plugins
+export default withPlugins(
+  [
+    // Configuración de PWA
+    withPWA,
+  ],
+  nextConfig
 );
--- Terra-Trade/pages/index.js
+++
@@ -1,4 +0,0 @@
-export default function Home() {
-    return <h1>¡Bienvenido a Terra Trade!</h1>;
-  }
-  
